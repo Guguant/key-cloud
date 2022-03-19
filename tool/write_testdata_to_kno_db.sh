527b3fb1c24053c9baed6fed1a302284a7bd0525
Sun Yiming <zscandyz@gmail.com>
2022-03-19 14:06:17 +0800

    update testdata

--- tool/write_testdata_to_kno_db.sh
+++ tool/write_testdata_to_kno_db.sh
@@ -24,14 +24,18 @@ sqlite3 kno.db \
 "INSERT INTO lockrealstatus (sroomId, sPosition, sOpen, sIP, sPort, sFacus) \
                      VALUES ('201', '6教中', 'on', '192.168.6.1', '8000', 0)" \
 "INSERT INTO lockrealstatus (sroomId, sPosition, sOpen, sIP, sPort, sFacus) \
-                     VALUES ('302', '6教中', 'off', '192.168.1.23', '8000', 0)";
+                     VALUES ('208', '6教南', 'off', '192.168.5.4', '8000', 0)" \
+"INSERT INTO lockrealstatus (sroomId, sPosition, sOpen, sIP, sPort, sFacus) \
+                     VALUES ('302', '6教中', 'off', '192.168.1.23', '8000', 0)" \
+"INSERT INTO lockrealstatus (sroomId, sPosition, sOpen, sIP, sPort, sFacus) \
+                     VALUES ('304', '6教南', 'off', '192.168.5.1', '8000', 1)";
 
 # newestversion.table
 sqlite3 kno.db \
 "INSERT INTO newestversion (pNewFunciton, pVersionHistory, pVersionCurrent, \
         pVersionReleaseTime, pPublishCompany, pSize, pCompatibility, \
         pLanguage, pDeveloper, pPrivacyPolicy) \
-        VALUES ('- real read sensors' status - opt web pages',\
+        VALUES ('- real read sensors status - opt web pages',\
                 'history.shtml', \
                 '1.1.20', \
                 '2017-10-30',\
@@ -43,4 +47,9 @@ sqlite3 kno.db \
                 'terms')";
 
 # user.table
-sqlite3 kno.db "INSERT INTO user (username, passwd) VALUES ('admin', 'admin')"
+sqlite3 kno.db "INSERT INTO user (username, passwd) VALUES ('admin', 'admin')";
+
+# log.table
+sqlite3 kno.db \
+        "INSERT INTO log (time, info) VALUES ('2022-03-19 00:41:40', 'added a new device, roomid-304 ip-192.168.5.1')"  \
+        "INSERT INTO log (time, info) VALUES ('2022-03-19 00:43:16', 'added a new device, roomid-208 ip-192.168.5.4')";
